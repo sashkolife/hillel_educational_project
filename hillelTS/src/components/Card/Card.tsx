0a8dcbdf21ffab49ebe68038f5cab38a41c3124f
Alexander.Dev <sashko.life@gmail.com>
2023-11-15 12:17:48 +0100

    fixes with codereview

--- hillelTS/src/components/Card/Card.tsx
+++ hillelTS/src/components/Card/Card.tsx
@@ -1,32 +1,37 @@
+import React from "react";
 import {UserModel} from "../../models/models.tsx";
 
-const Card = (props: {userInfo:UserModel}) => {
-    const userInfo: UserModel = props.userInfo;
-    const isModuleComplete: boolean = userInfo.currentModule === userInfo.course.maxModules;
-    const percentage: number = !isModuleComplete ? Math.ceil(userInfo.currentModule/userInfo.course.maxModules*100) : 0;
+interface ICardProps {
+    userInfo: UserModel;
+}
+
+const Card: React.FC<ICardProps> = ({userInfo}) => {
+    const {currentModule, course, id, level, rating, photo, fullName, time }:UserModel = userInfo;
+    const isModuleComplete: boolean = currentModule === course.maxModules;
+    const percentage: number = !isModuleComplete ? Math.ceil(currentModule/course.maxModules*100) : 0;
 
     return (
         <div className="card">
             <div className="image__container">
                 <img
-                    src={userInfo.course.courseImage}
+                    src={course.courseImage}
                     alt=""
                 />
-                <div className="level">{userInfo.level}</div>
+                <div className="level">{level}</div>
             </div>
 
-            <h3 className="card__title">{userInfo.course.courseTitle}</h3>
+            <h3 className="card__title">{course.courseTitle}</h3>
 
             <div className="card__info">
                 <div className="user">
                     <img
-                        src={userInfo.photo}
-                        alt={userInfo.fullName}
+                        src={photo}
+                        alt={fullName}
                     />
-                    <p>{userInfo.fullName}</p>
+                    <p>{fullName}</p>
                 </div>
                 <div className="rating">
-                    <p>{userInfo.rating}</p>
+                    <p>{rating}</p>
                 </div>
             </div>
 
@@ -36,16 +41,16 @@ const Card = (props: {userInfo:UserModel}) => {
                             {percentage}%
                         </progress>
                         <div className="card__info">
-                            <p>{userInfo.currentModule} / {userInfo.course.maxModules} Modules</p>
+                            <p>{currentModule} / {course.maxModules} Modules</p>
                             <div>{percentage} %</div>
                         </div>
                     </div>
             }
 
             <div className="card__info">
-                <div>{userInfo.id} Student</div>
-                <div>{userInfo.currentModule} Modules</div>
-                <div>{userInfo.time}</div>
+                <div>{id} Student</div>
+                <div>{currentModule} Modules</div>
+                <div>{time}</div>
             </div>
         </div>
     );
