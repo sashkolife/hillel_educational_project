cf879ac1e1cec334ff13bf64d5e8fff8c22b7bca
Alexander.Dev <sashko.life@gmail.com>
2023-11-16 10:58:34 +0100

    useState test

--- hillelTS/src/App.tsx
+++ hillelTS/src/App.tsx
@@ -1,15 +1,22 @@
-import React from "react";
-import { v4 as uuidv4 } from 'uuid';
-import {UserModel} from "./models/models.tsx";
-import Card from "./components/Card/Card.tsx";
-import {students} from "./data/data.tsx";
+import React, {useState} from "react";
 
 function App(): React.JSX.Element {
+
+    const [count, setCount] = useState(0);
+
+    const handleMinusClick = () => {
+        setCount(count-1);
+    };
+
+    const handlePlusClick = () => {
+        setCount(count+1);
+    };
+
     return (
         <div>
-            {students.map((student:UserModel) => (
-                <Card userInfo={student} key={uuidv4()}/>
-            ))}
+            <p>Count: {count}</p>
+            <button onClick={handleMinusClick}>Decrement</button>
+            <button onClick={handlePlusClick}>Increment</button>
         </div>
     )
 }
